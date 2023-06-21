64e4f3a9481ad84abc8ab772ec548e43b03035d9
Dirk Bolte <dirk.bolte@gmx.de>
2023-06-21 07:47:23 +0200

    Adding license headers + build

--- src/test/java/com/github/dirkbolte/wiremock/state/StateTest.java
+++ src/test/java/com/github/dirkbolte/wiremock/state/StateTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 Dirk Bolte
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.dirkbolte.wiremock.state;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -6,7 +21,6 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.extension.Parameters;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.commons.lang3.RandomStringUtils;
